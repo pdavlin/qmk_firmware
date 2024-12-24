ad378b767de2317c4659fbf3e90ff1f92f4eda1e
Patrick Davlin <patrick@davlin.io>
2024-12-24 13:11:46 -0600

    add keyboard pet

--- keyboards/pdavlin/sofle/keymaps/pd/rules.mk
+++ keyboards/pdavlin/sofle/keymaps/pd/rules.mk
@@ -1,7 +1,8 @@
-MOUSEKEY_ENABLE = yes
-EXTRAKEY_ENABLE = yes
-CONSOLE_ENABLE = no
-RGBLIGHT_ENABLE = yes
-ENCODER_ENABLE = yes
-LTO_ENABLE = yes
-OLED_ENABLE = yes
+MOUSEKEY_ENABLE = no			# Mouse keys (+1482). Not required for trackball usage.
+EXTRAKEY_ENABLE = yes			# Audio control and System control (+450)
+CONSOLE_ENABLE = no				# Console for debug (+4686)
+RGBLIGHT_ENABLE = yes			# RGB lighting (+3826)
+ENCODER_ENABLE = yes			# Rotary encoders (+1112)
+LTO_ENABLE = yes				# Save space by link time optimization
+OLED_ENABLE = yes				# Must be disabled if OLEDs are not installed (+3608-4774 with pet)
+WPM_ENABLE = yes				# WPM counter for typing-speed-reactive keyboard pets (+782)
