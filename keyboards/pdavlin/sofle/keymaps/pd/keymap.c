a7df083f5f89b3e08a224818ad7b2090bcb34026
Patrick Davlin <patrick@davlin.io>
2024-12-24 23:14:27 -0600

    fix numpad and numrow rgb macros

--- keyboards/pdavlin/sofle/keymaps/pd/keymap.c
+++ keyboards/pdavlin/sofle/keymaps/pd/keymap.c
@@ -25,30 +25,19 @@
 #define HSV_OVERRIDE(hsv, Override) HSV_OVERRIDE_HELP(hsv, Override)
 
 // Light combinations
-#define SET_INDICATORS(hsv) {0, 1, hsv}
-#define SET_UNDERGLOW(hsv) {0, 6, hsv}, {35 + 0, 6, hsv}
 #define SET_NUMPAD(hsv)                     \
-    {35 + 15, 5, hsv}, {35 + 22, 3, hsv}, { \
-        35 + 27, 3, hsv                     \
+    {35 + 13, 8, hsv}, { \
+        35 + 23, 3, hsv                     \
     }
-#define SET_NUMROW(hsv)                                                               \
-    {10, 2, hsv}, {20, 2, hsv}, {30, 2, hsv}, {35 + 10, 2, hsv}, {35 + 20, 2, hsv}, { \
-        35 + 30, 2, hsv                                                               \
-    }
-#define SET_INNER_COL(hsv) \
-    {33, 4, hsv}, {        \
-        35 + 33, 4, hsv    \
+#define SET_NUMROW(hsv)                                                                                                \
+    {11, 1, hsv}, {12, 1, hsv}, {21, 2, hsv}, {30, 2, hsv}, {35 + 11, 1, hsv}, {35 + 12, 1, hsv}, {35 + 21, 2, hsv}, { \
+        35 + 30, 2, hsv                                                                                                \
     }
 
-#define SET_OUTER_COL(hsv) \
-    {7, 4, hsv}, {         \
-        35 + 7, 4, hsv     \
+#define SET_LAYER_ID(hsv) \
+    {0, 7, hsv}, {        \
+        35 + 0, 7, hsv    \
     }
-#define SET_THUMB_CLUSTER(hsv) \
-    {25, 2, hsv}, {            \
-        35 + 25, 2, hsv        \
-    }
-#define SET_LAYER_ID(hsv) {0, 7, hsv}, {35 + 0, 7, hsv}
 
 enum sofle_layers { _DEFAULTS = 0, _QWERTY = 0, _COLEMAK, _COLEMAKDH, _LOWER, _RAISE, _ADJUST, _NUMPAD, _SWITCH };
 
